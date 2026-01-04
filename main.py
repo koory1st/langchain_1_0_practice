fee1067df232c94a2c6ef18a28614770efd56138
levy <levy@test.com>
2026-01-04 10:02:25 +0800

    rag

--- main.py
+++ main.py
@@ -1,66 +1,103 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_community.embeddings import MiniMaxEmbeddings
+
+from langchain.agents import create_agent
+from langchain.agents.middleware import before_model, after_model,AgentState
+
+load_dotenv(override=True)
+
+MINIMAX_API_KEY = os.getenv("MINIMAX_API_KEY")
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+
+from langchain_community.document_loaders import PyPDFLoader
+
+embeddings = MiniMaxEmbeddings()
+
+# 对话模型
 from langchain_deepseek import ChatDeepSeek
 from dotenv import load_dotenv
 import os
 
 from langchain.agents import create_agent
-from langchain.agents.middleware import SummarizationMiddleware
+from langchain.agents.middleware import before_model, after_model,AgentState
 
 load_dotenv(override=True)
 
-DEEPSEEK_API = os.getenv("DEEPSEEK_API")
-
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 model = ChatDeepSeek(
     model="deepseek-chat",
     temperature=0,
     max_tokens=None,
     timeout=None,
     max_retries=2,
-    api_key=DEEPSEEK_API,
     # other params...
 )
 
-from langchain.messages import SystemMessage, HumanMessage, AIMessage
-system_message = "你叫小智，是一名乐于助人的智能助手。请在对话中保持温和，有耐心的语气。"
-system_message = SystemMessage(content=system_message)
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
 
-messages = [
-    system_message
-]
+# Only keep post title, headers, and content from the full HTML.
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
 
 
-agent = create_agent(
-    model=model,
-    system_prompt="你是一名多才多艺的智能助手，可以调用工具帮助用户解决问题。",
-    middleware=[
-        SummarizationMiddleware(
-            model=model,
-            trigger=("tokens", 20),
-            keep=("messages", 2),
-            summary_prompt="请简要总结以下对话内容，保留关键信息，以便后续使用：\n{messages}",
-        )
-    ],
+from langchain_core.vectorstores import InMemoryVectorStore
+
+vector_store = InMemoryVectorStore(embeddings)
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+
+from langchain.tools import tool
+
+@tool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+from langchain.agents import create_agent
+
+
+tools = [retrieve_context]
+# If desired, specify custom instructions
+prompt = (
+    "You have access to a tool that retrieves context from a blog post. "
+    "Use the tool to help answer user queries."
+)
+agent = create_agent(model, tools, system_prompt=prompt)
+
+query = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
 )
 
-messages.append(HumanMessage(content="我叫陈明"))
-result = agent.invoke({
-    "messages": messages,
-})
-print(result)
-messages.append(AIMessage(content=result["messages"][-1].content))
-messages.append(HumanMessage(content="我20岁"))
-result = agent.invoke({
-    "messages": messages,
-})
-print(result)
-messages.append(AIMessage(content=result["messages"][-1].content))
-messages.append(HumanMessage(content="我希望你叫小天"))
-result = agent.invoke({
-    "messages": messages,
-})
-print(result)
-messages.append(AIMessage(content=result["messages"][-1].content))
-messages.append(HumanMessage(content="你叫什么"))
-result = agent.invoke({
-    "messages": messages,
-})
-print(result)
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": query}]},
+    stream_mode="values",
+):
+    event["messages"][-1].pretty_print()
